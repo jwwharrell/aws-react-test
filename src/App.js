39a4bfac24e3b99a7d2fe2c46716144981fa1a4e
Jordan Harrell <Jordan@Jordans-MacBook-Pro.local>
2019-12-07 21:53:10 -0500

    Fixed a path error

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
-import Header from './component/Header.js'
-import Main from './component/Main.js'
-import Footer from './component/Footer.js'
+import Header from './components/Header.js'
+import Main from './components/Main.js'
+import Footer from './components/Footer.js'
 
 function App() {
   return (
