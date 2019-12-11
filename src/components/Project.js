d42689dcfba67e622d5c9e33c824dbc86ce39c1c
Jordan Harrell <Jordan@Jordans-MacBook-Pro.local>
2019-12-10 22:49:16 -0500

    Added target param to anchor tags

--- src/components/Project.js
+++ src/components/Project.js
@@ -7,8 +7,8 @@ export default class Project extends Component {
             <div className='project-card'>
                 <h3>{this.props.title}</h3>
                 <p className='proj-links'>
-                    <a href={this.props.gitHub}>GitHub</a>
-                    <a href={this.props.appLink}>{this.props.title} App</a>
+                    <a href={this.props.gitHub} target="_blank">GitHub</a>
+                    <a href={this.props.appLink} target="_blank">{this.props.title} App</a>
                 </p>
                 <p>{this.props.description}</p>
 
