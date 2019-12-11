f53ad49ea7501b2de64140873458fbddd458f98d
Jordan Harrell <Jordan@Jordans-MacBook-Pro.local>
2019-12-11 12:45:54 -0500

    Made the site more mobile friendly

--- src/components/Project.js
+++ src/components/Project.js
@@ -7,8 +7,8 @@ export default class Project extends Component {
             <div className='project-card'>
                 <h3>{this.props.title}</h3>
                 <p className='proj-links'>
-                    <a href={this.props.gitHub} target="_blank">GitHub</a>
-                    <a href={this.props.appLink} target="_blank">{this.props.title} App</a>
+                    <a href={this.props.gitHub} target="_blank" rel="noopener noreferrer">GitHub</a>
+                    <a href={this.props.appLink} target="_blank" rel="noopener noreferrer">{this.props.title} App</a>
                 </p>
                 <p>{this.props.description}</p>
 
