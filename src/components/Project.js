3884bb22376b381c44df19225720535c05a6e3e1
Jordan Harrell <Jordan@Jordans-MacBook-Pro.local>
2020-01-10 15:59:26 -0500

    Updated project cards

--- src/components/Project.js
+++ src/components/Project.js
@@ -5,14 +5,16 @@ import 'animate.css'
 export default class Project extends Component {
     render() {
         return (
-            <div className='project-card'>
-                <h3>{this.props.title}</h3>
-                <p className='proj-links'>
-                    <a href={this.props.gitHub} target="_blank" rel="noopener noreferrer">GitHub</a>
-                    <a href={this.props.appLink} target="_blank" rel="noopener noreferrer">{this.props.title} App</a>
-                </p>
-                <p>{this.props.description}</p>
+            <div>
+                <div className='project-card'>
+                    <h3>{this.props.title}</h3>
+                    <p className='proj-links'>
+                        <a href={this.props.gitHub} target="_blank" rel="noopener noreferrer">GitHub</a>
+                        <a href={this.props.appLink} target="_blank" rel="noopener noreferrer">{this.props.title} App</a>
+                    </p>
+                    <p>{this.props.description}</p>
 
+                </div>
             </div>
         )
     }
--- src/components/ProjectCarousel.js
+++ src/components/ProjectCarousel.js
@@ -1,5 +1,10 @@
 import React, { Component } from 'react'
-import Project from './Project.js'
+import ProjectCard from './ProjectCard.js'
+import SantoIMG from '../images/screenshots/santo_portofino_ss.png'
+import PlacesIMG from '../images/screenshots/all_the_places_ss.png'
+import FashionIMG from '../images/screenshots/fashion_forward_ss.png'
+import AmbientIMG from '../images/screenshots/AmbientJam_ss.png'
+
 
 
 export default class ProjectCarousel extends Component {
@@ -7,46 +12,39 @@ export default class ProjectCarousel extends Component {
         return (
             <div className="pc-wrap">
                 <h2>Projects</h2>
-                <div className='project-container'>
-                    <div className='top-project-group'>
-                        <Project
+                        <ProjectCard
                             number='1'
                             title='Santo Portofino'
                             description="A variation of Simon Says. In my version of the game, there are five buttons (each representing a different not) that light up and play a tone in the same key as the accompanying background music."
-                            image=''
+                            image={SantoIMG}
                             gitHub='https://github.com/jwwharrell/Project-1-Game'
                             appLink='http://santo-portofino.surge.sh/'
                         />
 
-                        <Project
+                        <ProjectCard
                             number='2'
                             title='All the Places'
                             description='A travel app that catalogues museums and national parks'
-                            image=''
+                            image={PlacesIMG}
                             gitHub='https://github.com/jwwharrell/Project-2-App'
                             appLink='https://project-travel-app.herokuapp.com/'
                         />
-                    </div>
-                    <div className='bottom-project-group'>
-                        <Project
+                        <ProjectCard
                             number='3'
                             title='fashion forward'
                             description="A CRM tool for wardrobe stylists. Inspired by my brother-in-law/'s work, I wanted to find a way to distill his operation into one space."
-                            image=''
+                            image={FashionIMG}
                             gitHub='https://github.com/jwwharrell/Project-3-App'
                             appLink='https://fashion-forward-app.herokuapp.com/'
                         />
-
-                        <Project
+                        <ProjectCard
                             number='4'
                             title='Ambient Jam'
                             description='An app that allows you to make music with others! Upon entering the site, users can play the synth and hear everyone else who is currently playing.'
-                            image=''
+                            image={AmbientIMG}
                             gitHub='https://github.com/jwwharrell/Project-4-App'
                             appLink='https://master.d1huw8oejjdm3j.amplifyapp.com/'
                         />
-                    </div>
-                </div>
             </div>
         )
     }
