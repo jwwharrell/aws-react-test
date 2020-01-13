412c6de9ac3bd10a29e792c549b1a9282b66f9cd
Jordan Harrell <Jordan@Jordans-MacBook-Pro.local>
2020-01-13 18:12:21 -0500

    Formatting landing page card

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -2,21 +2,51 @@ import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
 import Container from 'react-bootstrap/Container'
 import Row from 'react-bootstrap/Row'
+import Card from 'react-bootstrap/Card'
 
 export default class LandingPage extends Component {
     render() {
         return (
-            <div>
-                <Container>
-                    <h1>Welcome</h1>
-                    <Row className="justify-content-center">
-                        <Link
-                            to='/stager'
-                            style={{
-                                "text-align": "center"
-                            }}
-                        >ENTER</Link>
-                    </Row>
+            <div
+                className='view'
+                style={{
+                    "background-image": "url('https://images.unsplash.com/photo-1461397821064-32d6b3c91b9f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80')",
+                    "background-repeat": "no-repeat",
+                    "background-size": "cover",
+                    "background-position": "center center"
+                }}
+            >
+                <Container
+                style={{
+                    "height": "100%"
+                }}
+                className='justify-content-center align-items-center'
+                
+                >
+                    <Card 
+                    style={{ 
+                        width: '18rem',
+                        'margin': '0 auto'
+                         }}
+                    bg='danger'
+                    >
+                        <Card.Body>
+                            <Card.Title
+                                style={{
+                                    "color": "#849D84"
+                                }}
+                            >Welcome</Card.Title>
+                            <Card.Text>
+                                My name is Jordan, come check out my work!
+                            </Card.Text>
+                            <Link
+                                to='/stager'
+                                style={{
+                                    "text-align": "center"
+                                }}
+                            >ENTER</Link>
+                        </Card.Body>
+                    </Card>
                 </Container>
             </div>
         )
