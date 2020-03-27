c1521e2228352b36c1373b86660a0f2a51cd93fc
Jordan Harrell <jwharrell0516@gmail.com>
2020-03-27 11:47:26 -0400

    Fixed broken LinkedIn link.

--- src/components/Header.js
+++ src/components/Header.js
@@ -10,7 +10,7 @@ export default class Header extends Component {
                         <img src={require("../images/profilish-pic-square.jpg")} alt="Jordan's face." />
                         <h2>Jordan Wadsworth-Harrell</h2>
                         <div className="social-media">
-                            <a href="https://www.linkedin.com/in/jordan-harrell-37abaa68" target="_blank" rel="noopener noreferrer" className="fa fa-linkedin"><span className="sr-only">Jordan's Linked In</span></a>
+                            <a href="https://www.linkedin.com/in/jordan-wadsworth-harrell-37abaa68/" target="_blank" rel="noopener noreferrer" className="fa fa-linkedin"><span className="sr-only">Jordan's Linked In</span></a>
                             <a href="https://github.com/jwwharrell" target="_blank" rel="noopener noreferrer" className="fa fa-github"><span className="sr-only">Jordan's GitHub</span></a>
                             <a href="mailto: jwharrell0516@gmail.com" target="_blank" rel="noopener noreferrer" className="fa fa-envelope"><span className="sr-only">Email Jordan</span></a>
                         </div>
